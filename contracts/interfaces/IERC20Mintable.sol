52da6b8b73f97bf7667dd9699c3192e2c7473826
Guilherme Viana <ggviana@hotmail.com.br>
2020-08-04 13:52:38 -0300

    Changed name to match interface name pattern and filename

--- contracts/interfaces/IERC20Mintable.sol
+++ contracts/interfaces/IERC20Mintable.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.8;
 
-interface ERC20Mintable {
+interface IERC20Mintable {
     function mint(uint256 amount) external returns (bool);
 
     function transfer(address recipient, uint256 amount) external returns (bool);
