7f19446d2b056f35f8f7589b17eb3dcd3105abc3
Guilherme Viana <ggviana@hotmail.com.br>
2020-08-04 14:02:11 -0300

    Marking base Option as abstract

--- contracts/PodOption.sol
+++ contracts/PodOption.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.6.8;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract PodOption is ERC20 {
+abstract contract PodOption is ERC20 {
     enum OptionType { PUT, CALL }
 
     OptionType public optionType;
